d2894ce3b74b8fd96831b340d9930d8562029a15
Christopher Acornley <c516792@uad.ac.uk>
2017-08-25 10:49:47 +0100

    Split into StarterProject and FinishedExample

--- StartProject/Assets/Scripts/PlayerCharacterController.cs
+++ StartProject/Assets/Scripts/PlayerCharacterController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCharacterController : MonoBehaviour {
 
     float maxSpeed = 10f;
-    float movement = 0f;
+    float score = 0f;
     bool facingRight = true;
 
 	// Use this for initialization
@@ -17,11 +18,11 @@ public class PlayerCharacterController : MonoBehaviour {
 	void Update () {
         float move = Input.GetAxis("Horizontal");
 
+        Animator animatorComponent = this.GetComponent<Animator>();
+        animatorComponent.SetFloat("moveSpeed", Mathf.Abs(move));
+
         Rigidbody2D rigidbody = this.GetComponent<Rigidbody2D>();
         rigidbody.velocity = new Vector2(move * maxSpeed, rigidbody.velocity.y);
-
-        Animator animator = this.GetComponent<Animator>();
-        animator.SetFloat("moveSpeed", Mathf.Abs(move));
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
@@ -46,8 +47,37 @@ public class PlayerCharacterController : MonoBehaviour {
         transform.localScale = scale;
     }
 
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        print("On Trigger Entered.");
+        // Try and find the 'ScoreText' GameObject
+        GameObject scoreTextObject = GameObject.Find("ScoreText");
+
+        // Get the Text component from the GameObject
+        Text scoreTextComponent = scoreTextObject.GetComponent<Text>();
+
+        // Update our score variable
+        score += 10f;
+
+        // Set the text of the 'ScoreText' GameObject to display the score
+        scoreTextComponent.text = "Score: " + score;
+
+        // Now that we have updated out score and out ScoreText - we can delete the trigger
+        Destroy(collider.gameObject);
     }
+
+    // Lets comment this out so it doesnt do anything for the time being
+    /*
+        void OnTriggerExit2D(Collider2D collider)
+        {
+            print("I have stopped colliding with a trigger");
+        }
+    */
+
+    // Lets comment this out so it doesnt do anything for the time being
+    /*
+        void OnTriggerStay2D(Collider2D collider)
+        {
+            print("I am colliding with a trigger");
+        }
+    */
 }
